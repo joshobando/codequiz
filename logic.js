e0ac5c336537ea4917fdd0f03f5ed91330ba54b6
accubility <joshua.obando98@gmail.com>
2020-10-30 18:57:23 -0400

    changed quiz title and added intro to quiz

--- logic.js
+++ logic.js
@@ -1,3 +1,12 @@
+// make an array
+// need objects
+// event listener to start quiz and buttons
+// the event.listener will tell you if the selection is right and the same for the wrong answer
+// has to reveal answer and keep score, but it needs to go away for the next questio
+// 
+// 
+// 
+// 
 var button = document.querySelector("#button")
 
 
